36b6b549f62564221a5a493e40bb719a9ccffa17
Souvik <souvikbanerjee241@gmail.com>
2023-09-16 13:03:11 +0530

    sql work

---
+++ limit.sql
@@ -0,0 +1,17 @@
+************************************ limit & offset ***********************************************
+
+***********************syntax of limit********************************** 
+
+select * from table_name where <condition> limit 2;
+
+which means show only two  data from a perticular table which are satisfy the certain condition.
+
+
+ 
+**************************syntax of offset and limit***************************
+
+ 
+select * from table_name where <condition> limit 2,4;
+
+which means it show  the data from 3rd position and showing more four data from a perticular table which are satisfy the certain condition.
+
---
+++ null_not_null.sql
@@ -0,0 +1,29 @@
+select * from sem5_student2 where degree is null;
+
+        ID NAME                                  AGE G PHONE      CITY                 DEGREE
+---------- ------------------------------ ---------- - ---------- -------------------- ----------
+         1 souvik                                 18 m 1236547890 chinsurah
+         2 susmita                                18 f 4569871230 chinsurah
+         3 ma                                     21 f 7896321450 kolkata
+         4 baba                                   56 m 8529637410 kolkata
+         5 dada                                   60 m 7869451230 serampur
+         6 mam                                    50 f 3217896540 serampur
+         7 kaku                                   50 m 1020304050 bombay
+         8 mantu                                  26 f 2589637420 bombay
+
+
+
+
+ select * from sem5_student2 where name is not null;
+
+        ID NAME                                  AGE G PHONE      CITY                 DEGREE
+---------- ------------------------------ ---------- - ---------- -------------------- ----------
+         1 souvik                                 18 m 1236547890 chinsurah
+         2 susmita                                18 f 4569871230 chinsurah
+         3 ma                                     21 f 7896321450 kolkata
+         4 baba                                   56 m 8529637410 kolkata
+         5 dada                                   60 m 7869451230 serampur
+         6 mam                                    50 f 3217896540 serampur
+         7 kaku                                   50 m 1020304050 bombay
+         8 mantu                                  26 f 2589637420 bombay
+
