eeec7dafc3e636e889e83d743577e965af8f803e
Souvik <souvikbanerjee241@gmail.com>
2023-09-16 11:25:41 +0530

    sql work

---
+++ regular_expression.sql
@@ -0,0 +1,27 @@
+^ --> '^ra'--> Beginning of string.
+
+
+$ --> 'an$'--> end of string.
+
+
+[...] -->'[rms]'-->any character listed between the square brackets.
+
+(IF ONLY r/m/s is presents in any of the data then only can those data will be displayed)
+
+
+
+
+
+^[...] --> '^[rms]'--> begins with any character listed between the square     		      brackets.
+
+
+(it returns the data which are started with either r or m or s)
+
+
+
+[a-z]-->'[a-h]e'--> match with the range.
+
+
+(it checks the pattern with ae,he)
+
+p1|p2|p3--> 'tom|dick|harry'--> matches any of the patterns p1,p2,or p3.
